47ae032d62bf67f9261800ac58d5e3799835ba98
besmaerteralb <denis.mimini.1@outlook.com>
2022-02-10 22:57:09 +0100

    adding the page and the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
+import { useEffect, useState } from 'react'
 
 import './App.css'
 
+
+export type QuotesType = {
+  id: number
+  philosopher: string
+  philosophy: string
+}
+
 function App() {
 
+  //state for all the quotes
+  const [quotes, setQuotes] = useState<QuotesType[]>([])
+  //state for one quote
+  const [quote, setQuote] = useState<QuotesType[]>([])
+
+
+
+
+  // Fetching the server (All quotes)
+  useEffect(() => {
+    fetch(`http://localhost:1234/quotes`)
+      .then(resp => resp.json())
+      .then(serverQuotes => setQuotes(serverQuotes))
+  }, [])
+
+
+  // Fetching only one quote based on id
+
+  useEffect(() => {
+    fetch(`http://localhost:1234/quotes/:id`)
+      .then(resp => resp.json())
+      .then(serverQuotes => setQuotes(serverQuotes))
+  }, [])
 
   return (
     <div className="App">
+      {
+        quotes.map(quote =>
+          <Quotes key={quote.id} quote={quote} />
+
+        )}
 
     </div>
   )

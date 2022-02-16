cdbcfc8d7a2cd7a1368565534b608b78f4e6cc0e
besmaerteralb <denis.mimini.1@outlook.com>
2022-02-16 09:37:18 +0100

    fixing some bug

--- src/pages/SingleQuote.tsx
+++ src/pages/SingleQuote.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { QuoteType } from "../App"
 
@@ -9,21 +10,25 @@ export default function SingleQuote({ quotes }: props) {
 
     const params = useParams()
 
-    const quote = quotes.find((filtredQuote) => filtredQuote.id === Number(params.id));
+    const [quote, setQuote] = useState<QuoteType | null>(null)
+    useEffect(() => {
+        fetch(`http://localhost:1234/quotes/${params.id}`)
+            .then(resp => resp.json())
+            .then(quoteFromServer => setQuote(quoteFromServer))
+    }, [])
 
-    if (quote !== undefined)
-        return (
-            <section>
-                <img
-                    src={quote.image}
-                    alt={quote.firstName}
-                />
-                <div>
-                    <h2>{quote.philosopy}</h2>
-                    <p>{quote.philosopher} </p>
-                    <p>{quote.age}</p>
-                </div>
+    return (
+        <section>
+            <img
+                src={quote?.image}
+                alt={quote?.firstName}
+            />
+            <div>
+                <h2>{quote?.philosopy}</h2>
+                <p>{quote?.philosopher} </p>
+                <p>{quote?.age}</p>
+            </div>
 
-            </section>
-        )
+        </section>
+    )
 }
